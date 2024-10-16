fc67f3a47fafb6daf09fefca941c05bebc33d6f5
Deucexmachina <dwaynevmartin@gmail.com>
2024-10-17 05:52:13 +0800

    10/17/24

--- ClassSample/Main.java
+++ ClassSample/Main.java
@@ -102,6 +102,8 @@ public class Main {
                 System.out.println("Department: " + emps[i].getDepartment());
                 System.out.println("---------------------------------------------");
             }
+            
         }
+
     }
 }
